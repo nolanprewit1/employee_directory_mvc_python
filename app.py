f4b297b20f8b3bc4ec4b8a017be55c0bea3e4222
Parker Prewit <parkerp@nbp.int>
2019-01-09 20:21:18 -0600

    working database select all call with index display

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 ### IMPORT REQUIRED PYTHON MODULES ###
 import json
+import sqlite3
+from sqlite3 import Error
 from flask import Flask, render_template, request
 
 ### IMPORT CONFIG FILE ### 
@@ -9,12 +11,70 @@ with open("config.json") as config_file:
 ### DEFINE THE FLASK APP ###
 server = Flask(__name__)
 
+### ALLOW RELOAD OF TEMPLATE FILES DURING DEVELOPMENT ###
+def before_request():
+    server.jinja_env.cache = {}
+server.before_request(before_request)
+
+### CONNECT TO THE DATABASE ###
+def connect_database():
+    try:
+        return sqlite3.connect(config.get("database_file"))
+    except:
+        print("There was an error connecting to the database...")
+
+### DEFINE SQL QUERY FUNCTIONS FOR EACH QUERY TYPE ###
+
+# CREATE
+# def create_query(query):
+#     cur = db_connection.cursor() 
+#     cur.execute(query)
+#     rows = cur.fetchall()
+#     return rows
+
+# READ
+def select_all_query(query):
+    db_connection = connect_database()
+    cur = db_connection.cursor() 
+    cur.execute(query)
+    rows = cur.fetchall()
+    db_connection.close()
+    return rows
+
+# READ
+def select_query(query, var):
+    db_connection = connect_database()
+    cur = db_connection.cursor() 
+    cur.execute(query, var)
+    rows = cur.fetchall()
+    db_connection.close()
+    return rows
+
+# UPDATE
+def update_query(query, var):
+    db_connection = connect_database()
+    cur = db_connection.cursor() 
+    cur.execute(query, var)
+    db_connection.commit()
+    db_connection.close()
+
+# DELETE
+def delete_query(query, var):
+    db_connection = connect_database()
+    cur = db_connection.cursor() 
+    cur.execute(query, var)
+    db_connection.close()
+
 ### DEFINE THE ROUTES ###
 @server.route("/")
 @server.route("/index")
 def index():
-    return render_template('index.html')
+    results = select_all_query("SELECT * FROM EMPLOYEES")
+    return render_template("index.html", results=results)
 
 ### START AND RUN THE SERVER ###
-if __name__ == "__main__":    
-    server.run(debug=False,host='0.0.0.0',port=config.get("web_port"))
+if __name__ == "__main__": 
+    server.debug = False
+    server.run(host="0.0.0.0",port=config.get("web_port"))
+    
+    

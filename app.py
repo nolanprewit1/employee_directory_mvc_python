6f8ea7f94d9356b9cbebced9d5dbdee401695a37
Parker Prewit <parkerp@nbp.int>
2019-01-10 21:16:22 -0600

    Working employee detail page with back button

--- app.py
+++ app.py
@@ -74,18 +74,18 @@ def employee_create():
         employee_last_name = request.form['employee_last_name']
         employee_position = request.form['employee_position']
         create_update_query(
-            'INSERT INTO employee (first_name,last_name,position) VALUES (?,?,?)', 
+            'INSERT INTO EMPLOYEE (first_name,last_name,position) VALUES (?,?,?)', 
             (employee_first_name,employee_last_name,employee_position)
         )
         return redirect("/", code=302)
     else:
         return redirect("/", code=302)
 
-@server.route("/employee/<id>", methods=["GET"])
+@server.route("/employee/detail/<id>", methods=["GET"])
 def employe_detail(id):
     if request.method == "GET":
-        results = select_query("SELECT * FROM EMPLOYEE WHERE id = ?", id)
-        return render_template("employe_detail.html", results=results)
+        results = select_query('SELECT * FROM EMPLOYEE WHERE id = ?', (id,))
+        return render_template("employee_detail.html", results=results)
     else:
         return redirect("/", code=302)
 

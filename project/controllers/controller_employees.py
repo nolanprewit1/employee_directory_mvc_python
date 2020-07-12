cb7ffb4b974aae8c001726a9d0f89d1e16115410
Parker Prewit <nolanprewit1@gmail.com>
2020-07-11 20:11:22 -0500

    working update route

--- project/controllers/controller_employees.py
+++ project/controllers/controller_employees.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request, redirect, send_file
 from project import app
 from project.models import model_employees
 from project import db_connection
+from sqlalchemy import update
 
 ### DEFINE THE ROUTES ###
 # CREATE
@@ -25,58 +26,49 @@ def employee_create():
 # READ
 @app.route("/employees")
 def employees_index():
-    results = db_connection.query(model_employees.Employees)\
+    results = db_connection\
+        .query(model_employees.Employees)\
         .all()
     return render_template("employees/index.html", results=results)
 
 @app.route("/employees/detail/<id>", methods=["GET"])
 def employees_detail(id):
-        # results = select_query(
-        #     'SELECT * FROM EMPLOYEE WHERE id = ?', 
-        #     (id,)
-        # )
-        # results = db_connection.query(data)\
-        #     .filter(employees.id==id)
-    #     session.query(User)\
-    # .filter(Address.email_address=='ed@google.com')\
-    # .first()
-    results = db_connection.query(model_employees.Employees)\
+    results = db_connection\
+        .query(model_employees.Employees)\
         .filter(model_employees.Employees.id==id)
     return render_template("employees/detail.html", results=results)
 
-#UPDATE
-# @app.route("/employee/update/<id>", methods=["GET", "POST"])
-# def employee_update(id):
-#     if request.method == "GET":
-#         results = select_query(
-#             'SELECT * FROM EMPLOYEE WHERE id = ?', 
-#             (id,)
-#         )
-#         return render_template("employee_update.html", results=results)
-#     elif request.method == "POST":
-#         employee_first_name = request.form['employee_first_name']
-#         employee_last_name = request.form['employee_last_name']
-#         employee_position = request.form['employee_position']
-#         print(id)
-#         create_update_query(
-#             'UPDATE EMPLOYEE SET first_name = ?, last_name = ?, position = ? where id = ?',
-#             (employee_first_name,employee_last_name,employee_position,id)
-#         )
-#         return redirect("/", code=302)
-#     else:
-#         return redirect("/", code=302)
+# UPDATE
+@app.route("/employees/update/<id>", methods=["GET", "POST"])
+def employee_update(id):
+    if request.method == "GET":
+        results = db_connection\
+            .query(model_employees.Employees)\
+            .filter(model_employees.Employees.id==id)
+        return render_template("employees/update.html", results=results)
+    elif request.method == "POST":
+        results = db_connection\
+            .query(model_employees.Employees)\
+            .get(id)
+        results.firstName = request.form['firstName']
+        results.lastName = request.form['lastName']
+        results.position = request.form['position']
+        db_connection.commit()
+        return redirect("/", code=302)
+    else:
+        return redirect("/", code=302)
 
-#DELETE
-# @app.route("/employee/delete/<id>", methods=["GET", "POST"])
-# def employee_delete(id):
-#     if request.method == "GET":
-#         results = select_query('SELECT * FROM EMPLOYEE WHERE id = ?', (id,))
-#         return render_template("employee_delete.html", results=results)
-#     elif request.method == "POST":
-#         delete_query(
-#             'DELETE FROM EMPLOYEE WHERE ID = ?',
-#             (id,)
-#         )
-#         return redirect("/", code=302)
-#     else:
-#         return redirect("/", code=302)
+# DELETE
+@app.route("/employees/delete/<id>", methods=["GET", "POST"])
+def employee_delete(id):
+    if request.method == "GET":
+        results = select_query('SELECT * FROM EMPLOYEE WHERE id = ?', (id,))
+        return render_template("employee_delete.html", results=results)
+    elif request.method == "POST":
+        delete_query(
+            'DELETE FROM EMPLOYEE WHERE ID = ?',
+            (id,)
+        )
+        return redirect("/", code=302)
+    else:
+        return redirect("/", code=302)
